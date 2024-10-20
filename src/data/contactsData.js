cf7319b66855c414520dac24647aff54fac4dc71
paridhi agarwal <107147060+pari5313@users.noreply.github.com>
2024-10-20 21:03:14 +0530

    Update contactsData.js

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,5 +1,5 @@
 export const contactsData = {
-    email: 'ParidhiAgarwal.test@gmail.com',
+    email: 'ParidhiAgarwal129@gmail.com',
     phone: '+917310789844',
     address: ' Ghaziabad, Uttar Pradesh, India. ',
 
